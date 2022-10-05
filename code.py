4ed89309039864d4aee06a3dcb1b567cea3c3da1
Simarjot Singh (Monga) <simar5244@gmail.com>
2022-10-05 19:18:34 +0530

    base

---
+++ code.py
@@ -0,0 +1,39 @@
+https://drive.google.com/file/d/1TcXkVFXcxhoX5YEJBq1_3wOsSFY8qTPI/view?usp=sharing
+  
+import warnings
+import itertools
+import numpy as np
+import matplotlib.pyplot as plt
+warnings.filterwarnings("ignore")
+plt.style.use('fivethirtyeight')
+import pandas as pd
+import statsmodels.api as sm
+import matplotlib
+matplotlib.rcParams['axes.labelsize'] = 14
+matplotlib.rcParams['xtick.labelsize'] = 12
+matplotlib.rcParams['ytick.labelsize'] = 12
+matplotlib.rcParams['text.color'] = 'k'
+
+df = pd.read_excel("Superstore.xls")
+furniture = df.loc[df['Category'] == 'Furniture']
+furniture['Order Date'].min(), furniture['Order Date'].max()
+
+cols = ['Row ID', 'Order ID', 'Ship Date', 'Ship Mode', 
+        'Customer ID', 'Customer Name', 
+        'Segment', 'Country', 'City', 'State', 
+        'Postal Code', 'Region', 'Product ID', 
+        'Category', 'Sub-Category', 'Product Name', 
+        'Quantity', 'Discount', 'Profit']
+furniture.drop(cols, axis=1, inplace=True)
+furniture = furniture.sort_values('Order Date')
+furniture.isnull().sum()
+
+furniture = furniture.groupby('Order Date')['Sales'].sum().reset_index()
+
+from pylab import rcParams
+rcParams['figure.figsize'] = 18, 8
+decomposition = sm.tsa.seasonal_decompose(y, model='additive')
+fig = decomposition.plot()
+plt.show()
+
+
